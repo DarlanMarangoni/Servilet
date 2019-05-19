9f7b16a6d47d11ab4a8890c8eff3d00af03578b0
darlanmarangoni <marangoni.darlan1@gmail.com>
2019-05-19 13:24:19 -0300

    Removendo empresa

--- WebContent/listaEmpresas.jsp
+++ WebContent/listaEmpresas.jsp
@@ -16,7 +16,11 @@
 	<ul>	
 		<c:forEach items="${empresas}" var="empresa">
 			
-			<li>${empresa.nome} - <fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy"/> </li>
+			<li>
+				${empresa.nome} - <fmt:formatDate value="${empresa.dataAbertura}" pattern="dd/MM/yyyy" /> 
+				<a href="/gerenciador/removeEmpresa?id=${empresa.id}">remove</a>
+				<a href="/gerenciador/alteraEmpresa">editar</a>
+			</li>
 		</c:forEach>
 	</ul>
 
--- src/br/com/alura/gerenciador/servlet/Banco.java
+++ src/br/com/alura/gerenciador/servlet/Banco.java
@@ -1,6 +1,7 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Banco {
@@ -11,8 +12,10 @@ public class Banco {
 	static {
 		Empresa empresa = new Empresa();
 		empresa.setNome("Alura");
+		empresa.setId(Banco.id++);
 		Empresa empresa2 = new Empresa();
 		empresa2.setNome("Caelum");
+		empresa2.setId(Banco.id++);
 		lista.add(empresa);
 		lista.add(empresa2);
 	}
@@ -25,6 +28,18 @@ public class Banco {
 	
 	public List<Empresa> getEmpresas(){
 		return Banco.lista;
+	}
+	
+	public void remove(int id) {
+		Iterator<Empresa> it = lista.iterator();
+		//utilizando iterator por que arraylist nao deixar remover 
+		//o elemento que esta sendo  iterado
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			if (emp.getId() == id) {
+				it.remove();
+			}
+		}
 	}
 	
 }
